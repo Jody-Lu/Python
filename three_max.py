a1d6aebb881195cb08ca5090e16a5b09650f2da2
Jody-Lu <b00901192@ntu.edu.tw>
2016-01-26 15:35:17 +0800

    Update three_max.py

--- three_max.py
+++ three_max.py
@@ -1,7 +1,16 @@
-with open("input.txt", 'r+') as f:
-	lines = [line[:-1] for line in f]
+import sys
 
-for i in range(0,3):
-	print lines[i]
+def three_max(in_file):
+	if in_file == '':
+		in_file = sys.argv[1]
+
+	with open(in_file, 'r+') as f:
+		lines = [line[:-1] for line in f]
+		result = []
+		max_1 = max(lines)
+		max_2 = max(n for n in lines if n != max_1)
+		max_3 = max(n for n in lines if n != max_2 and n != max_1)
+		result = [max_1, max_2, max_3]
+	return result
 
 
