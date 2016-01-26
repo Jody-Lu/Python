4ad62a7ad4f5b42270dc57d164c6b0a26360d0e5
Jody-Lu <b00901192@ntu.edu.tw>
2016-01-26 17:12:31 +0800

    Update three_max.py

--- three_max.py
+++ three_max.py
@@ -1,16 +1,30 @@
 import sys
 
-def three_max(in_file):
-	if in_file == '':
-		in_file = sys.argv[1]
-
-	with open(in_file, 'r+') as f:
+def readfile(file_name):
+	if file_name == '':
+		file_name = sys.argv[1]
+	with open(file_name, 'r+') as f:
 		lines = [line[:-1] for line in f]
-		result = []
-		max_1 = max(lines)
-		max_2 = max(n for n in lines if n != max_1)
-		max_3 = max(n for n in lines if n != max_2 and n != max_1)
-		result = [max_1, max_2, max_3]
+	return lines
+
+def three_max(lines):
+	tmp = lines
+	result = ['']*3
+	result[0] = max(tmp)
+
+	del tmp[tmp.index(max(tmp))]
+
+	if tmp[0] > tmp[1]:
+		result[1], result[2] = tmp[0], tmp[1]
+	else:
+		result[1], result[2] = tmp[1], tmp[0]
+
+	for x in tmp[2:]:
+		if x > result[2]:
+			if x > result[1]:
+				result[2], result[1] = result[1], x
+			else:
+				result[2] = x
 	return result
 
 
