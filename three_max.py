7b9650c8cf0ae4d48bb7cff3779ad46a775fa21d
Jody-Lu <b00901192@ntu.edu.tw>
2016-01-27 13:37:50 +0800

    Update three_max.py

--- three_max.py
+++ three_max.py
@@ -4,19 +4,16 @@ def readfile(file_name):
 	if file_name == '':
 		file_name = sys.argv[1]
 	with open(file_name, 'r+') as f:
-		lines =  [int(line.rstrip()) for line in f] # rstrip() can remove the '\n' in sting
+		numbers =  [int(number.rstrip()) for number in f] # rstrip() can remove the '\n' in sting
 
- 	return lines
+ 	return numbers
 
-def three_max(nums):
-	print nums
+def three_max(numbers):
 	result = [0]*3
-	for i in range(0, len(nums)):
-		for j in range(0, len(result) - 1):
-			if nums[i] > result[j]:
-				result = [nums[i], result[j], result[j+1]]
-				print result
-				break
+	for index in range(0, len(numbers)):
+		if numbers[index] > result[0]:
+			result.insert(0, numbers[index])
+			result.pop()
 
 	return result
 
