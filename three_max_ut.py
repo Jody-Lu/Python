2834b9f53b725d50fdd8c9d7b2a89fe85f3f681a
Jody-Lu <b00901192@ntu.edu.tw>
2016-01-27 11:57:55 +0800

    Update three_max_ut.py

--- three_max_ut.py
+++ three_max_ut.py
@@ -3,12 +3,14 @@ import three_max
 
 class TmaxTest(unittest.TestCase):
 	def test1(self):
+		read = three_max.readfile
 		Tmax = three_max.three_max
-		self.assertEqual(Tmax("input.txt"), ['9239', '8723', '6421'])
+		self.assertEqual(Tmax(read("input.txt")), [9239, 8723, 6421])
 
 	def test2(self):
+		read = three_max.readfile
 		Tmax = three_max.three_max
-		self.assertEqual(Tmax("input.txt"), ['923', '872', '642'])
+		self.assertEqual(Tmax(read("input.txt")), [923, 872, 642])
 
 if __name__ == '__main__':
 	unittest.main()
