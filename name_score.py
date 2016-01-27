0e8ba1406aa03cef9abae5095f3ddeaae5acbfcf
Jody-Lu <b00901192@ntu.edu.tw>
2016-01-27 12:19:00 +0800

    Update name_score.py

--- name_score.py
+++ name_score.py
@@ -6,21 +6,18 @@ def readfile(file_name):
 		file_name = sys.argv[1]
 	with open(file_name, 'r+') as f:
 		tmp = f.readline()
-		lines = [word.strip('"') for word in tmp.split(',')]
-	return lines
+		names = [word.strip('"') for word in tmp.split(',')]
+	return names
 
 
-def name_score(lines):
-	score = [0] * len(lines)
-	total_score = 0
-	lines.sort()
-
-	for i, word in enumerate(lines, 0):
-		score[i] = sum([ord(x) - 64 for x in word])
-
-	total_score = sum([(i + 1) * score[i] for i in range(0, len(score))])
+def name_score(names):
+	names.sort()
+	total_score = sum([sum([ord(x) - 64 for x in name]) \
+					* (i + 1) for i, name in enumerate(names, 0)])
 
 	return total_score
+
+
 
 if __name__ == '__main__':
 	print name_score(readfile("input2.dat"))
